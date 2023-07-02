a47f18c849999a35cc09afa55cb120fe2412740f
jaceuk <jnewington@gmail.com>
2023-07-02 12:41:13 +0100

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
-    format: "file",
-  },
+	build: {
+		// Example: Generate `page.html` instead of `page/index.html` during build.
+		format: 'file'
+	},
+	site: 'https://www.evo-plumbingandheating.co.uk/',
+	server: { port: 3001, host: true }
 });

81d70a46d54a1869f4d58bad54b8d9bd05f0289f
jaceuk <jnewington@gmail.com>
2023-07-03 20:03:33 +0100

    Checkatrade

--- utils/utils.js
+++ utils/utils.js
@@ -1,35 +1,36 @@
 export function getServiceReviews(reviews, serviceSlug) {
-  const keywords = [];
+	const keywords = [];
 
-  if (serviceSlug === 'plumbing') keywords.push('leak', 'tap', 'shower', 'bath', 'pump', 'overflow', 'water');
-  if (serviceSlug === 'blocked-sink-drains') keywords.push('block', 'sink', 'basin', 'toilet');
-  if (serviceSlug === 'central-heating') keywords.push('heating', 'radiator');
-  if (serviceSlug === 'power-flushing') keywords.push('power', 'flush');
-  if (serviceSlug === 'boiler-servicing-repairs') keywords.push('boiler');
-  if (serviceSlug === 'gutter-cleaning') keywords.push('gutter');
-  if (serviceSlug === 'patio-cleaning') keywords.push('patio');
-  if (serviceSlug === 'roof-cleaning') keywords.push('roof');
+	if (serviceSlug === 'plumbing')
+		keywords.push('leak', 'tap', 'shower', 'bath', 'pump', 'overflow', 'water', 'sink', 'water');
+	if (serviceSlug === 'blocked-sink-drains') keywords.push('block', 'sink', 'basin', 'toilet');
+	if (serviceSlug === 'central-heating') keywords.push('heating', 'radiator');
+	if (serviceSlug === 'power-flushing') keywords.push('power', 'flush');
+	if (serviceSlug === 'boiler-servicing-repairs') keywords.push('boiler');
+	if (serviceSlug === 'gutter-cleaning') keywords.push('gutter');
+	if (serviceSlug === 'patio-cleaning') keywords.push('patio');
+	if (serviceSlug === 'roof-cleaning') keywords.push('roof');
 
-  let filteredReviews = [];
-  reviews.forEach((review) => {
-    const title = review.title.toLowerCase();
+	let filteredReviews = [];
+	reviews.forEach((review) => {
+		const title = review.title.toLowerCase();
 
-    keywords.forEach((keyword) => {
-      if (title.includes(keyword)) {
-        filteredReviews.push(review);
-      }
-    });
-  });
+		keywords.forEach((keyword) => {
+			if (title.includes(keyword)) {
+				filteredReviews.push(review);
+			}
+		});
+	});
 
-  // remove duplicates
-  return unique(filteredReviews);
+	// remove duplicates
+	return unique(filteredReviews);
 }
 
 export function getLatestReviews(reviews, limit) {
-  unique(reviews).sort((a, b) => new Date(b.date) - new Date(a.date));
-  return reviews.slice(0, limit);
+	unique(reviews).sort((a, b) => new Date(b.date) - new Date(a.date));
+	return reviews.slice(0, limit);
 }
 
 export function unique(array) {
-  return [...new Set(array)];
+	return [...new Set(array)];
 }

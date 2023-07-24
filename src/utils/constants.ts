bfee3ec81d532bad2b238583bc4bfc46dfac694b
jaceuk <jnewington@gmail.com>
2023-07-24 20:14:24 +0100

    Trying to fix server issue with Build command

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,10 @@
-export const WEBSITE = import.meta.env.PUBLIC_WEBSITE;
-export const API_URL = import.meta.env.PUBLIC_API_URL;
-export const RECAPTCHA_SITE_KEY = import.meta.env.PUBLIC_RECAPTCHA_SITE_KEY;
+export const WEBSITE = 'plumbing';
+
+export const API_URL =
+	import.meta.env.PUBLIC_ENVIRONMENT === 'development'
+		? 'https://api.evo-homeservices.co.uk/api'
+		: 'http://localhost:3000/api';
+
+export const RECAPTCHA_SITE_KEY = '6LcLQ9sUAAAAANQermDnySq_6PvjBm1piE43kUUA';
+
 export const FREEPHONE_NUMBER = '0800 920 2030';
